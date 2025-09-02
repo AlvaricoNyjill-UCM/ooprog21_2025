db99b958384f7425e856cc4035d197e60846860e
AlvaricoNyjill-UCM <135498573+AlvaricoNyjill-UCM@users.noreply.github.com>
2025-09-02 22:29:59 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single-line comment
+        System.out.println("Hello, this line is explained with a single-line comment.");
+        
+        /* 
+         * This is a multi-line comment.
+         * It can span multiple lines and is often used
+         * for explaining longer sections of code.
+         */
+        System.out.println("This line is explained with a multi-line comment.");
+        
+        /**
+         * This is a documentation comment.
+         * It is used to describe classes, methods, and variables.
+         * Tools like Javadoc use these comments to generate documentation.
+         */
+        System.out.println("This line is explained with a documentation comment.");
+    }
+}
