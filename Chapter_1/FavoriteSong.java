7fe5c925b34c2efc7a1366e6630d742962222518
AlvaricoNyjill-UCM <135498573+AlvaricoNyjill-UCM@users.noreply.github.com>
2025-09-02 22:18:47 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("All Girls Are The Same");
+        System.out.println("Movie Star");
+        System.out.println("Rather lie");
+        System.out.println("About You");
+    }
+}
